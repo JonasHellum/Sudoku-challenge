62caf54bf9a108ac6cafcc5710a188a84b2db5b7
Jonas Lærke Hellum <jonas_laerke@hotmail.com>
2024-03-11 09:49:17 +0100

    Added Sudoku.cs since the method was static inside Program.cs

--- SudokuChallenge/Program.cs
+++ SudokuChallenge/Program.cs
@@ -18,42 +18,10 @@ class Program
             ,['.','.','.','4','1','9','.','.','5']
             ,['.','.','.','.','8','.','.','7','9']];
         
-        bool isValid = IsValidSudoku(board);
+        Sudoku sudoku = new Sudoku();
+        bool isValid = sudoku.IsValidSudoku(board);
         Console.WriteLine("Valid: " + isValid);
     }
     
-    static bool IsValidSudoku(char[][] board)
-    {
-        HashSet<string> letter = new HashSet<string>();
-
-        for (int row = 0; row < 9; row++)
-        {
-            for (int col = 0; col < 9; col++)
-            {
-                if (board[row][col] != '.')
-                {
-                    string cellRow = "Row: " + (row + 1) + ", number: " + board[row][col];
-                    string cellCol = "Column: " + (col + 1) + ", number: " + board[row][col];
-                    string cellBox = "Box: " + (row/3 + 1) + "," + (col/3 + 1) + ", number: " + board[row][col];
-                    
-                    /*
-                    Console.WriteLine(cellRow);
-                    Console.WriteLine(cellCol);
-                    Console.WriteLine(cellBox);
-                    Console.WriteLine();
-                    */
-                    
-                    
-                    if (!letter.Add(cellRow) ||
-                        !letter.Add(cellCol) ||
-                        !letter.Add(cellBox))
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
+    
 }
---
+++ SudokuChallenge/Sudoku.cs
@@ -0,0 +1,39 @@
+﻿namespace SudokuChallenge;
+
+public class Sudoku
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        HashSet<string> letter = new HashSet<string>();
+
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row][col] != '.')
+                {
+                    string cellRow = "Row: " + (row + 1) + ", number: " + board[row][col];
+                    string cellCol = "Column: " + (col + 1) + ", number: " + board[row][col];
+                    string cellBox = "Box: " + (row/3 + 1) + "," + (col/3 + 1) + ", number: " + board[row][col];
+                    
+                    /*
+                    Console.WriteLine(cellRow);
+                    Console.WriteLine(cellCol);
+                    Console.WriteLine(cellBox);
+                    Console.WriteLine();
+                    */
+                    
+                    
+                    if (!letter.Add(cellRow) ||
+                        !letter.Add(cellCol) ||
+                        !letter.Add(cellBox))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
